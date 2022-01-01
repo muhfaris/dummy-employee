4887670d6ebf909d4f99a0be51286ec721fd905e
Muhammad Faris <muhfaris@disroot.org>
2022-01-01 10:34:40 +0700

    feat: debugging

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.post("/v1/create", (req, res) => {
+  console.log(req.body)
   res.jsonp({
     status: "success",
     data: req.body,
