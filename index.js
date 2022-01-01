f9860021d367f7fd796294e466bb71c2bdb1dbe3
Muhammad Faris <muhfaris@disroot.org>
2022-01-01 09:51:37 +0700

    fix: path dbjson

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // server.js
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router('db.json')
+const router = jsonServer.router('./db.json')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
